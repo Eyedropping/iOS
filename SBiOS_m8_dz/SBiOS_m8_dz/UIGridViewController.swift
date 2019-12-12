d008341dfa3f4699987ec93415eb6477c942cf45
Dmitry Aksyonov <dmi.aksyonov@gmail.com>
2019-12-13 01:25:08 +0300

    Update UIGridViewController.swift

--- SBiOS_m8_dz/SBiOS_m8_dz/UIGridViewController.swift
+++ SBiOS_m8_dz/SBiOS_m8_dz/UIGridViewController.swift
@@ -31,47 +31,37 @@ class UIGridViewController: UIViewController {
         var x = 20, y = 100, width = self.view.frame.width / 2 - 30, height = width * 1.3, yAxis = 0, labelNo = 0
         let bgColor = UIColor.systemPink
         let alpha: CGFloat = 0.8
+        let margins = self.view.layoutMarginsGuide
         
         func create2Views(xCoord: Int, yCoord: Int, frameWidth: Int, frameHeight: Int) {
-            let frame1 = CGRect(x: xCoord, y: yCoord, width: Int(frameWidth), height: Int(frameHeight))
-            let view1 = UIImageView(frame: frame1)
+            let viewFrame1 = CGRect(x: xCoord, y: yCoord, width: Int(frameWidth), height: Int(frameHeight))
+            let view1 = UIImageView(frame: viewFrame1)
             view1.backgroundColor = bgColor
             view1.alpha = alpha
-            view1.contentMode = .scaleAspectFit
-            view1.translatesAutoresizingMaskIntoConstraints = false
-//            let margins = self.view.layoutMarginsGuide
-//            view1.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
-            
-            
-            
-//            print(margins)
-//
-            
-            
-//            let view1HorizontalConstraint = NSLayoutConstraint(item: view1, attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view1, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: 20)
-//            let view1VerticalConstraint = NSLayoutConstraint(item: view1, attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view1, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 1, constant: 100)
-//            let view1WidthConstraint = NSLayoutConstraint(item: view1, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: self.view.frame.width / 2 - 30)
-//            let view1HeightConstraint = NSLayoutConstraint(item: view1, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: (self.view.frame.width / 2 - 30) * 1.3)
             self.view.addSubview(view1)
-//            self.view.addConstraints([view1HorizontalConstraint, view1VerticalConstraint, view1WidthConstraint, view1HeightConstraint])
             
-            let frame2 = CGRect(x: xCoord + 195, y: yCoord, width: Int(frameWidth), height: Int(frameHeight))
-            let view2 = UIImageView(frame: frame2)
+            let viewFrame2 = CGRect(x: xCoord + 195, y: yCoord, width: Int(frameWidth), height: Int(frameHeight))
+            let view2 = UIImageView(frame: viewFrame2)
             view2.backgroundColor = bgColor
             view2.alpha = alpha
-            view2.contentMode = .scaleAspectFit
-            
-//            let view2HorizontalConstraint = NSLayoutConstraint
-//            let view2VerticalConstraint = NSLayoutConstraint
-//            let view2WidthConstraint = NSLayoutConstraint
-//            let view2HeightConstraint = NSLayoutConstraint
             self.view.addSubview(view2)
         }
+        
+        //        func createLabel() {
+        //            let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
+        //            label.center = CGPoint(x: 160, y: 285)
+        //            label.textAlignment = NSTextAlignment.center
+        //            label.text = "Label #\(labelNo)"
+        //            self.view.addSubview(label)
+        //        }
         
         while yAxis < 2 {
             create2Views(xCoord: x, yCoord: y, frameWidth: Int(width), frameHeight: Int(height))
             y += 350
             yAxis += 1
         }
+        
+        //        createLabel()
+        print(margins.layoutFrame)
     }
 }
