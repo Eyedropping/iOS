1ea9b6389ccec756d046bbe45e698966109345c4
Dmitry Aksyonov <dmi.aksyonov@gmail.com>
2020-03-02 00:21:43 +0300

    Upload 14 module

---
+++ Skillbox/SBiOS_m14_dz_new/Pods/Target Support Files/Pods-SBiOS_m14_dz/Pods-SBiOS_m14_dz-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SBiOS_m14_dz : NSObject
+@end
+@implementation PodsDummy_Pods_SBiOS_m14_dz
+@end
---
+++ Skillbox/SBiOS_m14_dz_new/SBiOS_m14_dz/Model/CoreDataCell.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataCell.swift
+//  SBiOS_m14_dz
+//
+//  Created by Dmitry Aksyonov on 17.02.2020.
+//  Copyright © 2020 Dmitry Aksyonov. All rights reserved.
+//
+
+import UIKit
+
+class CoreDataCell: UITableViewCell {
+    @IBOutlet weak var toDoTextLabel: UILabel!
+    @IBOutlet weak var isDoneTextLabel: UILabel!
+}
